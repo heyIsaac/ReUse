50d80b8cb617bbca46b9d02fe0e56c26cbeeb226
Isaac Lima de Andrade <devzac.andrade@gmail.com>
2026-03-07 23:56:45 -0300

    remove .env from repository

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
-const BASE_URL = "http://localhost:5251/api";
+const BASE_URL = "https://reuse-9fal.onrender.com/api";
 
 export const api = axios.create({
   baseURL: BASE_URL,
