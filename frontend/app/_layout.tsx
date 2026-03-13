b026a8227daa5d2ad22e23038ba53b1ae4633ceb
Isaac Lima de Andrade <devzac.andrade@gmail.com>
2026-03-13 18:43:44 -0300

    chore: add react tanstack

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,34 +1,27 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-
-import { LogBox } from 'react-native';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { LogBox, useColorScheme } from "react-native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { useColorScheme } from "react-native";
 import "react-native-reanimated";
 import "../global.css";
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
-  // Lendo o tema (Light/Dark) diretamente do sistema operacional
   const colorScheme = useColorScheme();
   LogBox.ignoreLogs(['SafeAreaView has been deprecated']);
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      {/* headerShown: false remove as barras superiores nativas, permitindo o edge-to-edge */}
-      <Stack screenOptions={{ headerShown: false }}>
-        {/* Aqui mapeamos as rotas principais que criaremos */}
-        <Stack.Screen name="index" />
-        <Stack.Screen name="(auth)" />
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-
-      {/* translucent e style="auto" garantem que a barra se adapte ao fundo (claro/escuro) */}
-      <StatusBar style="auto" translucent />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(auth)" />
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+        <StatusBar style="auto" translucent />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
